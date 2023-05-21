4d706918a4e418a5eed77b93958c666acd40cb92
Raj Vikrant Brahma <114692462+Batmanrvb@users.noreply.github.com>
2023-05-21 12:19:52 +0530

    Add files via upload

---
+++ Statuses.py
@@ -0,0 +1,23 @@
+import os
+
+statutes_folder = r"C:\Users\Raj Vikrant Brahma\Desktop\cases_folder\Object_statutes"
+
+# Step 1: Read the statute information from text files
+statutes_data = []
+
+# Iterate over the text files in the statutes folder
+for file_name in os.listdir(statutes_folder):
+    file_path = os.path.join(statutes_folder, file_name)
+    with open(file_path, 'r', encoding='utf-8') as file:
+        statute_title = file.readline().strip()
+        statute_description = file.read().strip()
+        statutes_data.append({'title': statute_title, 'description': statute_description})
+
+# Step 2: Perform your task (e.g., retrieval or classification) using the statutes_data
+
+# Example: Print the statute information
+for statute in statutes_data:
+    print(statute['title'])
+    print(statute['description'])
+    print()
+
